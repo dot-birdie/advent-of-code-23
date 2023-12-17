485bb06ffb596faa85f265d710280a3bf84a4c88
Alexander Vogel <alexander.vogel2@student.kit.edu>
2023-12-17 15:30:54 +0100

    day 10 completed

--- day_10/task2.py
+++
@@ -1,8 +0,0 @@
-
-
-f = open("example1.txt", "r")
-# f = open("data.txt", "r")
-lines = f.readlines()
-
-for line in lines:
-    pass
--- day_10/tasks.py
+++ day_10/tasks.py
@@ -1,5 +1,8 @@
 import time
 import numpy as np
+import copy
+
+default_distance = -1
 
 directions = {
     "north": (-1, 0),
@@ -42,6 +45,7 @@ def find_starting_point(lines) -> tuple:
 
 def get_first_pipes(lines, starting_point):
     pipes = []
+    start_symbols = PIPES.keys()
     for direction in directions.values():
         position = np.add(starting_point, direction)
         # ensure the computed position is a valid index
@@ -51,9 +55,9 @@ def get_first_pipes(lines, starting_point):
         for pipe_directions in PIPES[symbol]:
             if np.array_equal(np.multiply(direction, -1), pipe_directions):
                 pipes.append(Node(position, direction, 1, lines))
+                start_symbols = list(filter(lambda key: direction in PIPES[key], start_symbols))
                 break
-    return pipes
-
+    return pipes, start_symbols[0]
 
 def get_next_node(lines, current_node):
     pipe_directions = PIPES[current_node.symbol]
@@ -62,23 +66,56 @@ def get_next_node(lines, current_node):
             return Node(np.add(current_node.position, pipe_dir), pipe_dir, current_node.distance + 1, lines)
 
 
+def calculate_inner_area(distances, lines):
+    area = 0
+    inner_area_mask = copy.deepcopy(lines)
+    for line_index in range(len(distances)):
+        inside_loop = False
+        loop_came_from_top = False
+        for element_index in range(len(distances[0])):
+            if distances[line_index][element_index] == default_distance:
+                # position was not part of the loop, depending on inside_loop boolean increment area counter
+                if inside_loop:
+                    area += 1
+                    inner_area_mask[line_index] = inner_area_mask[line_index][:element_index] + "I" \
+                        + inner_area_mask[line_index][element_index + 1:]
+            else:
+                symbol = lines[line_index][element_index]
+                if symbol == "|":
+                    inside_loop = not inside_loop
+                elif symbol == "-":
+                    continue
+                elif symbol == "L":
+                    loop_came_from_top = True
+                    inside_loop = not inside_loop
+                elif symbol == "F":
+                    loop_came_from_top = False
+                    inside_loop = not inside_loop
+                elif symbol == "J" and loop_came_from_top:
+                    inside_loop = not inside_loop
+                elif symbol == "7" and not loop_came_from_top:
+                    inside_loop = not inside_loop
+    return area
+
+
 start_time = time.time()
 
-# f = open("example2.txt", "r")
+# f = open("example5.txt", "r")
 f = open("data.txt", "r")
 lines = f.readlines()
 lines = list(map(lambda x: x.replace("\n", ""), lines))
 
 start_pos = find_starting_point(lines)
 
-distances = [[-1] * len(lines[0]) for _ in range(len(lines))]
+distances = [[default_distance] * len(lines[0]) for _ in range(len(lines))]
 distances[start_pos[0]][start_pos[1]] = 0
 
-current_nodes = get_first_pipes(lines, start_pos)
+current_nodes, start_symbol = get_first_pipes(lines, start_pos)
+lines[start_pos[0]] = lines[start_pos[0]][:start_pos[1]] + start_symbol + lines[start_pos[0]][start_pos[1] + 1:]
 
 while current_nodes:
     current = current_nodes.pop(0)
-    if distances[current.position[0]][current.position[1]] != -1:
+    if distances[current.position[0]][current.position[1]] != default_distance:
         break
     else:
         distances[current.position[0]][current.position[1]] = current.distance
@@ -86,8 +123,8 @@ while current_nodes:
     next_node = get_next_node(lines, current)
     current_nodes.append(next_node)
 
-# print(distances)
 maximal_distance = np.max(distances)
 print(f"Maximal distance in loop is: {maximal_distance}")
+print(f"Inner area of circle: {calculate_inner_area(distances, lines)}")
 end_time = time.time()
 print(f"Processing time: {end_time - start_time} seconds")
